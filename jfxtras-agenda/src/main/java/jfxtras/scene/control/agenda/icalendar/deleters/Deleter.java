aacbf965d8f3f88252b92c02cd015ecb1d5bf5bf
daviddbal <ddbal1@yahoo.com>
2016-10-19 09:19:42 -0700

    added some iCalendarAgenda files
    no compile error in Eclipse.  Gradle compileJava says error.  Why?

---
+++ jfxtras-agenda/src/main/java/jfxtras/scene/control/agenda/icalendar/deleters/Deleter.java
@@ -0,0 +1,36 @@
+package jfxtras.scene.control.agenda.icalendar.deleters;
+
+import java.util.List;
+
+import jfxtras.icalendarfx.VCalendar;
+
+/**
+ * Interface for the delete behavior of a VComponent
+ * 
+ * <p>Delete options include:
+ * <ul>
+ * <li>One
+ * <li>All
+ * <li>This-and-Future
+ * </ul>
+ * </p>
+ * 
+ * @author David Bal
+ *
+ */
+public interface Deleter
+{
+    /** Revise list of iTIP VCalendar components that represent the canceled calendar components. */
+    // TODO - DETERMINE IF A LIST IS REQUIRED OR JUST ONE VCALENDAR
+    List<VCalendar> delete();
+    
+ // TODO - ADD A INITIALIZE METHOD FOR AN ARRAY OF INPUT OBJECT PARAMETERS
+    
+//    public static VCalendar emptyCanceliTIPMessage()
+//    {
+//        return new VCalendar()
+//                .withMethod(MethodType.CANCEL)
+//                .withProductIdentifier(ICalendarAgenda.DEFAULT_PRODUCT_IDENTIFIER)
+//                .withVersion(new Version());
+//    }
+}
